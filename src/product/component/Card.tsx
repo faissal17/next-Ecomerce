954bc5b85d7218390130eef19c8fff5ec438f53c
faissal17 <faissalaoukacha@gmail.com>
2024-01-12 15:44:33 +0100

    add card style

---
+++ src/product/component/Card.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Card = () => {
+  return (
+    <div className="flex justify-center items-center h-96">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg transition-colors duration-200 hover:bg-gray-200">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+            excepturi sequi fugiat quia omnis. Deserunt harum eaque molestias
+            nam quae?
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
