3887268af42d934664c3e41cb59bd25137dd3160
faissal17 <faissalaoukacha@gmail.com>
2024-01-12 11:15:14 +0100

    add navbar to shared component

--- src/shared/component/Navbar.tsx
+++ src/shared/component/Navbar.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
-import { Icon } from 'next/dist/lib/metadata/types/metadata-types';
+import {
+  FaProductHunt,
+  FaTags,
+  FaShoppingCart,
+  FaClipboardList,
+} from 'react-icons/fa';
 
 const Navbar = () => {
   return (
@@ -20,36 +25,42 @@ const Navbar = () => {
         </a>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+            <div>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center font-bold text-xl py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  aria-current="page"
+                >
+                  <FaProductHunt className="mr-2" />
+                  Products
+                </a>
+              </li>
+            </div>
             <li>
               <a
                 href="#"
-                className="font-bold text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                aria-current="page"
+                className="flex items-center font-bold text-xl py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Products
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="font-bold text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
+                <FaTags className="mr-2" />
                 Categories
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="font-bold text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="flex items-center font-bold text-xl py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
+                <FaClipboardList className="mr-2" />
                 Orders
               </a>
             </li>
             <li>
               <a
                 href="#"
-                className="font-bold text-xl block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="flex items-center font-bold text-xl py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-900 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
+                <FaShoppingCart className="mr-2" />
                 Cart
               </a>
             </li>
